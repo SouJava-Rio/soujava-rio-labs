65c92ef3a1d66b81bf7085f51fde325fda689034
daniel-dos <daniel.dias.analistati@gmail.com>
2018-08-22 01:23:01 -0300

    add example IMC .

---
+++ JSR-385-UnitsofMeasurement/IMC/src/main/java/br/org/soujava/rio/imc/IMCDemo.java
@@ -0,0 +1,43 @@
+/*
+ * The MIT License
+ * Copyright © 2017 Daniel Dias
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package br.org.soujava.rio.imc;
+
+import tech.units.indriya.quantity.Quantities;
+import tech.units.indriya.unit.Units;
+
+/**
+ * @author <a href="mailto:daniel.dias@soujava.org.br">Daniel Dias</a>
+ * github:Daniel-Dos
+ * twitter:@danieldiasjava
+ */
+public class IMCDemo {
+	public static void main(String[] args) {
+
+		var massa = Quantities.getQuantity(50, Units.KILOGRAM);
+		var altura  = Quantities.getQuantity(1.60, Units.METRE);
+		
+		var imc = massa.divide(altura.multiply(altura));
+		
+		System.out.println(imc);
+	}
+}
