9f9bcded1297293bb900b7842e19803c7ecbd2fd
Daniel Dias <daniel.dias.analistati@gmail.com>
2019-02-07 02:47:01 -0200

    add port

--- microframeworks/pippo/pippo-hello/src/main/java/pippo/demo/AppMain.java
+++ microframeworks/pippo/pippo-hello/src/main/java/pippo/demo/AppMain.java
@@ -1,12 +1,16 @@
 package pippo.demo;
 
+import java.util.Optional;
+
 import ro.pippo.core.Pippo;
 
 public class AppMain {
     public static void main(String[] args) {
+        
+        Optional<String> port = Optional.ofNullable(System.getenv("PORT")); 
         var pippo = new Pippo(new MyApplication());
         pippo
         .setFilterPath("/api/*")
-        .start();
+        .start(Integer.parseInt(port.orElse("8080")));
     }
 }
