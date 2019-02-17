51bb4da1d82eddb4c2724dfa41126c741e9fbfb0
Daniel Dias <daniel.dias.analistati@gmail.com>
2019-02-17 02:09:52 -0300

    update

--- microframeworks/helidion-se/helidon-se/src/main/java/br/org/soujava/rio/helidon/main/App.java
+++ microframeworks/helidion-se/helidon-se/src/main/java/br/org/soujava/rio/helidon/main/App.java
@@ -5,21 +5,17 @@ import java.util.Optional;
 
 import javax.enterprise.event.Observes;
 import javax.inject.Inject;
-import javax.json.Json;
-import javax.json.JsonObject;
 
 import org.jboss.weld.environment.se.events.ContainerInitialized;
 
 import br.org.soujava.rio.helidon.model.Event;
 import br.org.soujava.rio.helidon.model.ResponseModel;
 import br.org.soujava.rio.helidon.repository.EventRepository;
+import io.helidon.media.jsonb.server.JsonBindingSupport;
 import io.helidon.webserver.Handler;
 import io.helidon.webserver.Routing;
 import io.helidon.webserver.ServerConfiguration;
-import io.helidon.webserver.ServerRequest;
-import io.helidon.webserver.ServerResponse;
 import io.helidon.webserver.WebServer;
-import io.helidon.webserver.json.JsonSupport;
 
 public class App {
 
@@ -32,29 +28,12 @@ public class App {
 
         var configuration = ServerConfiguration.builder().port(Integer.parseInt(port.orElse("8080"))).build();
 
-        WebServer.create(configuration,Routing.builder().register(JsonSupport.create())
-                        .post("/event", Handler.create(JsonObject.class, this::addEvent))
-                        .get("/event", this::getEventsAll  ))
+        WebServer.create(configuration,Routing.builder().register(JsonBindingSupport.create())
+                        .post("/events", Handler.create(Event.class, (req,res,events) -> {
+                            this.eventRepository.save(events);
+                            res.send(new ResponseModel(res.status().code(), "Evento salvo com Sucesso !"));
+                        })).get("/events", (req,res) -> res.send(eventRepository.findAll())))
+                        
                         .start();
-    }
-
-    private void addEvent(ServerRequest req, ServerResponse res, JsonObject json) {
-
-        var event = new Event(json.getString("name"), json.getString("description"), json.getString("speaker"));
-
-        this.eventRepository.save(event);
-
-        var builder = Json.createArrayBuilder();
-        builder.add(new ResponseModel(res.status().code(), "Event Added Successfully!").forRest());
-        
-        res.send(builder.build());
-    }
-
-    private void getEventsAll(ServerRequest req, ServerResponse res) {
-
-        var builder = Json.createArrayBuilder();
-        eventRepository.findAll().forEach(data -> builder.add(data.forRest()));
-
-        res.send(builder.build());
     }
 }
